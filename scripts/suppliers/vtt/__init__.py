f62d97c736d63639baacbb4d0dfe2daed6900d58
JimStyle-gb <JimStyle.88@mail.ru>
2026-04-03 18:39:14 +0500

    Delete scripts/suppliers/vtt/__init__.py

--- scripts/suppliers/vtt/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Path: scripts/suppliers/vtt/__init__.py
-"""
-
-from .builder import build_offer_from_raw  # noqa: F401
-from .models import ParsedProductPage, ProductIndexItem, VTTConfig  # noqa: F401
-from .source import cfg_from_env, collect_product_index, login, make_session, parse_product_page_from_index  # noqa: F401
